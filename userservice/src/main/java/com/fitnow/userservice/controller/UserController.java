0f9673f72cc53105fc685efee0e2c0294fd00c37
De-VANG <devangbhatnagar12@gmail.com>
2025-07-21 23:35:56 +0530

    Build the Service User Endpoints

--- userservice/src/main/java/com/fitnow/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitnow/userservice/controller/UserController.java
@@ -1,21 +1,28 @@
 package com.fitnow.userservice.controller;
 
+import com.fitnow.userservice.dto.RegisterRequest;
 import com.fitnow.userservice.dto.UserResponse;
+import com.fitnow.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@AllArgsConstructor
 public class UserController {
+
+    private UserService userService;
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
         return ResponseEntity.ok(userService.getUserProfile(userId));
     }
 
-    @GetMapping("/register")
-    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request){
-        return ResponseEntity.ok(request.register(request));
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request){
+        return ResponseEntity.ok(userService.register(request));
     }
 
 
